78782ec48057b0aed8b69a64ebfa5a0e97982376
Argentovskii <Argentovsky@gmail.com>
2026-06-20 12:13:49 +0300

    Домашние задание 2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormSet(BaseInlineFormSet):
+
+    def clean(self):
+        super().clean()
+
+        main_count = 0
+
+        for form in self.forms:
+            if not form.cleaned_data:
+                continue
+
+            if form.cleaned_data.get('DELETE'):
+                continue
+
+            if form.cleaned_data.get('is_main'):
+                main_count += 1
+
+        if main_count != 1:
+            raise ValidationError(
+                'Должен быть один и только один основной раздел'
+            )
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormSet
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
