5125f28cfbcde3f78a234db2947730c0a6334d97
Argentovskii <Argentovsky@gmail.com>
2026-06-20 09:26:11 +0300

    Homework 2.1

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -13,5 +13,13 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.update_or_create(
+                id=phone['id'],
+                defaults={
+                    'name': phone['name'],
+                    'image': phone['image'],
+                    'price': phone['price'],
+                    'release_date': phone['release_date'],
+                    'lte_exists': phone['lte_exists'] == 'True',
+                }
+            )
